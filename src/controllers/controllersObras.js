e0340ac3f958a4fcb42a4431013fe1e7e47144cd
VitoriaMariadev <vitoriamaria.rodrigues02@gmail.com>
2023-08-07 21:48:13 -0300

    FIX/ O back errou na pesquisa

--- src/controllers/controllersObras.js
+++ src/controllers/controllersObras.js
@@ -47,35 +47,31 @@ const MostrarObrasComNomeEIdUsuario = async (req, res) => {
 
   try {
     const obra = await pool.query(`
-    SELECT 
-    o.id_obra, o.titulo, o.data_publi, o.resumo, u.nome as usuario, 
-    ARRAY_TO_STRING(ARRAY_AGG(DISTINCT li.link), ', ') as links, 
-    ARRAY_TO_STRING(ARRAY_AGG(DISTINCT im.link), ', ') as imgs,
-    ARRAY_TO_STRING(ARRAY_AGG(DISTINCT ass.nome), ', ') as assuntos, 
-    ARRAY_TO_STRING(ARRAY_AGG(DISTINCT au.nome), ', ') as autores
-FROM 
-    obra o
-INNER JOIN 
-    obras_autores oa ON o.id_obra = oa.id_obra
-INNER JOIN 
-    autor au ON au.id_autor = oa.id_autor
-INNER JOIN 
-    usuario u ON u.id_usuario = o.id_usuario
-INNER JOIN obras_assuntos oas ON o.id_obra = oas.id_obra
-INNER JOIN assunto ass ON ass.id_assunto = oas.id_assunto
-INNER JOIN obras_links ol ON ol.id_obra = o.id_obra
-INNER JOIN link li ON li.id_link = ol.id_link
-INNER JOIN obras_imgs oi ON oi.id_obra = o.id_obra
-INNER JOIN img im ON im.id_img = oi.id_img
-WHERE 
-    o.titulo ILIKE '%' || '${titulo}' || '%'
-    and o.id_usuario = ${id_usuario}
-GROUP BY 
-    o.id_obra, o.titulo, o.resumo, u.nome, o.data_publi
-ORDER BY 
-    o.id_obra;
-
-    `);
+      SELECT 
+        o.id_obra, o.titulo, o.data_publi, o.resumo, u.nome as usuario, 
+        ARRAY_TO_STRING(ARRAY_AGG(DISTINCT li.link), ', ') as links, 
+        ARRAY_TO_STRING(ARRAY_AGG(DISTINCT im.link), ', ') as imgs,
+        ARRAY_TO_STRING(ARRAY_AGG(DISTINCT ass.nome), ', ') as assuntos, 
+        ARRAY_TO_STRING(ARRAY_AGG(DISTINCT au.nome), ', ') as autores
+      FROM 
+        obra o
+        INNER JOIN obras_autores oa ON o.id_obra = oa.id_obra
+        INNER JOIN autor au ON au.id_autor = oa.id_autor
+        INNER JOIN usuario u ON u.id_usuario = o.id_usuario
+        INNER JOIN obras_assuntos oas ON o.id_obra = oas.id_obra
+        INNER JOIN assunto ass ON ass.id_assunto = oas.id_assunto
+        INNER JOIN obras_links ol ON ol.id_obra = o.id_obra
+        INNER JOIN link li ON li.id_link = ol.id_link
+        INNER JOIN obras_imgs oi ON oi.id_obra = o.id_obra
+        INNER JOIN img im ON im.id_img = oi.id_img
+      WHERE 
+        o.titulo ILIKE $1
+        AND o.id_usuario = $2
+      GROUP BY 
+        o.id_obra, o.titulo, o.resumo, u.nome, o.data_publi
+      ORDER BY 
+        o.id_obra;
+    `, [`%${titulo}%`, id_usuario]);
 
     if (obra.rows.length === 0) {
       return res
@@ -90,6 +86,7 @@ ORDER BY
       .json({ mensagem: "Ocorreu um erro interno no servidor" });
   }
 }
+
 
 const MostrarObraPeloID = async (req, res) => {
   const { id } = req.params;
